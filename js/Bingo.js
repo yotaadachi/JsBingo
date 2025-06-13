00e5740b7dbe66d7db7795720ffe8ee3f41be029
Yota Adachi <yota961219@gmail.com>
2025-06-13 17:36:48 +0900

    modefied js

--- js/Bingo.js
+++ js/Bingo.js
@@ -27,7 +27,7 @@
     let btn0 = document.getElementById('btn0');
     let spinButton = document.getElementById('spinButton');
 
-    let soundManager = soundManager();
+    let soundManager = SoundManager();
 
     // 結果のレンダリング
     let renderBingo = function () {
@@ -47,9 +47,9 @@
     renderBingo();
 
     // SPINボタン押下処理
-    spinButton.addEventListener('click', function() {
-        if(isPlaying) return;
-    
+    spinButton.addEventListener('click', function () {
+        if (isPlaying) return;
+
         soundManager.playDrum();
         isPlaying = true;
 
@@ -59,11 +59,72 @@
         panel1.className = 'panel';
         panel10.className = 'panel';
 
+        runSlot(0, panel1);
+        runSlot(1, panel10);
     });
+
+    // SPINボタン押下後の処理
+    function runSlot(n, panel) {
+        let num = bingo[Math.floor(Math.random() * bingo.length)];
+        bingoNum = num;
+
+        // ランダムにセレクトされた数字を1の位と10の位で分割表示させる
+        let num1 = num.substring(0, 1);
+        let num10 = num.substring(1, 2);
+
+        // 表示
+        panel1.innerHTML = num1;
+        panel10.innerHTML = num10;
+
+        timers[n] = setTimeout(function () {
+            runSlot(n, panel)
+        }, 25);
+    }
+
+    btn0.addEventListener('click', function() {
+        stopSlot(0, panel1, panel10, this);
+    })
+
+    // STOPボタン押下後の処理
+    function stopSlot(n, panel1, panel10, btn) {
+        if (!isPlaying || results[n] !== undefined) return;
+        btn.className = 'btn inactive';
+
+        soundManager.stopDrum();
+        soundManager.playCymbal();
+
+        clearTimeout(timers[n]);
+        clearTimeout(timers[n + 1]);
+
+        // ランダムでセレクトされた数字bingoNumをBingo配列から削除
+        delArrayNum = bingo.indexOf(bingoNum);
+        if (delArrayNum >= 0) {
+            // 削除
+            bingo.splice(delArrayNum, 1);
+        }
+        stopCount++;
+
+        if (stopCount === 1) {
+            stopCount = 0;
+            spinButton.className = '';
+
+            let bingoDiv = document.querySelectorAll(".bingo");
+
+            // change color
+            bingoDiv[bingoNum - 1].innerHTML;
+            bingoDiv[bingoNum - 1].className = 'bingo unmatched';
+
+            // init
+            isPlaying = false;
+            spinButton.className = '';
+            timers = [];
+        }
+    }
+
     // mp3ファイルを実行するメソッド
     function SoundManager() {
         let drum = document.getElementById('audio_drum');
-        let cymbal= document.getElementById('audio_cymbal');
+        let cymbal = document.getElementById('audio_cymbal');
 
         return {
             playDrum: playDrum,
@@ -72,7 +133,7 @@
         }
 
         function playDrum() {
-            if(drum == null) {
+            if (drum == null) {
                 return;
             }
             drum.currentTime = 0;
@@ -80,14 +141,14 @@
         }
 
         function stopDrum() {
-            if(drum == null) {
+            if (drum == null) {
                 return;
             }
             drum.pause();
         }
 
         function playCymbal() {
-            if(cymbal == null) {
+            if (cymbal == null) {
                 return;
             }
             cymbal.currentTime = 0;
